f445a9fb2a16d4f2b22db6fc25f847359d07fe05
Poomrapee Chaiprsop <poomrapee.cps@gmail.com>
2023-06-26 12:45:51 +0700

    version update

---
+++ java/api/src/main/java/com/exadel/frs/core/trainservice/controller/ConsistenceController.java
@@ -0,0 +1,33 @@
+package com.exadel.frs.core.trainservice.controller;
+
+import static com.exadel.frs.core.trainservice.system.global.Constants.API_V1;
+import com.exadel.frs.commonservice.sdk.faces.FacesApiClient;
+import com.exadel.frs.commonservice.system.global.ImageProperties;
+import com.exadel.frs.core.trainservice.dto.VersionConsistenceDto;
+import com.exadel.frs.core.trainservice.service.EmbeddingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(API_V1 + "/consistence")
+@RequiredArgsConstructor
+public class ConsistenceController {
+
+    private final EmbeddingService embeddingService;
+    private final FacesApiClient facesApiClient;
+    private final ImageProperties imageProperties;
+
+    @GetMapping("/status")
+    public VersionConsistenceDto getCheckDemo() {
+        var calculatorVersion = facesApiClient.getStatus().getCalculatorVersion();
+
+        return VersionConsistenceDto
+                .builder()
+                .demoFaceCollectionIsInconsistent(embeddingService.isDemoCollectionInconsistent())
+                .dbIsInconsistent(embeddingService.isDbInconsistent(calculatorVersion))
+                .saveImagesToDB(imageProperties.isSaveImagesToDB())
+                .build();
+    }
+}
